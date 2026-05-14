19b6867338569540057adbd607cf6c8cf72fe4b9
dermayor <123976489+dermayor@users.noreply.github.com>
2026-05-14 19:56:51 +0300

    config file bug
    Signed-off-by: dermayor <123976489+dermayor@users.noreply.github.com>

--- src/lib/device_manager/device_manager.cpp
+++ src/lib/device_manager/device_manager.cpp
@@ -301,16 +301,20 @@ std::vector<mantis::params::msdr_params> mantis::device_manager::get_connected()
 
 void mantis::device_manager::configure() {
     const char* config_file_raw = std::getenv("MANTIS_CONFIG_FILE");
+    std::string config_file;
     if (config_file_raw == nullptr) {
+
         config_file_raw = std::getenv("HOME");
-    }
+        if (config_file_raw == nullptr) {
+            throw mantis::runtime_error(
+                "failes to parse env vars MANTIS_CONFIG_FILE and HOME, cannot search for cofig file");
+        }
 
-    if (config_file_raw == nullptr) {
-        throw mantis::runtime_error(
-            "failes to parse env vars MANTIS_CONFIG_FILE and HOME, cannot search for cofig file");
-    }
+        config_file = std::string(config_file_raw) + "/.config/mantis/mantis-config.json";
 
-    const std::string config_file = std::string(config_file_raw) + "/.config/mantis/mantis-config.json";
+    } else {
+        config_file = config_file_raw;
+    }
 
     std::ifstream mantis_config_json(config_file);
     if (!mantis_config_json.is_open()) {
