98fdef5fd8dec3c1e5fbd8a2b9b7c077c6d7697f
dermayor <123976489+dermayor@users.noreply.github.com>
2026-05-18 11:00:03 +0300

    added note on rx channel
    Signed-off-by: dermayor <123976489+dermayor@users.noreply.github.com>

--- include/mantis/driver_impl/virtual/socket/virtual_rx_channel_socket.h
+++ include/mantis/driver_impl/virtual/socket/virtual_rx_channel_socket.h
@@ -2,20 +2,22 @@
 #include <mantis/driver_impl/virtual/socket/net_utils/msocket.h>
 #include <mantis/lib/driver_specific/interfaces/i_rx_channel.h>
 
-
 namespace mantis::driver_impl::virtual_i {
     /**
      * @brief virtual rx from socket class
      */
     class virtual_rx_channel_socket : public virtual_channel<interfaces::i_rx_channel> {
-    public:
+      public:
         virtual_rx_channel_socket(const params::msdr_params& _params, params::mchannel_params _channel_params);
 
         ~virtual_rx_channel_socket() override = default;
 
-        void receive(char *data, size_t sample_size, size_t num_samples, go::mrx_metadata &rx_metadata) override;
+        /**
+         * @note The socket channel does not currently support receiving less than
+         */
+        void receive(char* data, size_t sample_size, size_t num_samples, go::mrx_metadata& rx_metadata) override;
 
-    private:
+      private:
         net::msocket sock;
     };
-}
+} // namespace mantis::driver_impl::virtual_i
