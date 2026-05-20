7973e8f8b8caf051e16b32ac2da97e6ffefa29b1
dermayor <123976489+dermayor@users.noreply.github.com>
2026-05-20 16:06:41 +0300

    minor cmake updates, better messaging
    Signed-off-by: dermayor <123976489+dermayor@users.noreply.github.com>

--- cmake/MantisConfig.cmake
+++ cmake/MantisConfig.cmake
@@ -4,3 +4,5 @@ find_dependency(Threads REQUIRED)
 find_dependency(nlohmann_json REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/MantisTargets.cmake)
+
+message(STATUS "Found Mantis: ${mantis_VERSION}")
